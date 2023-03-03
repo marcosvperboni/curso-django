ca8cd9eab8f1788c37e5244ce7cea1e9bda198cf
Marcos Valerio Perboni <marcosvperboni@gmail.com>
2023-03-03 20:12:02 -0300

    Salvando

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
     return render(request, 'recipes/home.html', {
         'name': 'Marcos Perboni'
     })
-
-def contato(request):
-    return HttpResponse('contao 2 ')
-
-def sobre(request):
-    return HttpResponse('sobre 2 ')
 
 
